0df623f99fb11f9123042d5cc5547ad8386985b1
Mike Pullman <Morphuemax@gmail.com>
2021-12-12 16:04:55 -0500

    Initial Commit

--- src/main/py/generator.py
+++ src/main/py/generator.py
@@ -5,20 +5,19 @@ import shutil
 import chevron
 
 
-
 def compile_enum_data(data):
     all_enums = {}  # Create empty dict which we will fill and return
     for key in data:
         isEnum = False  # Assume it's not an enum until known
-        if data[key].get('enum') is not None:   # Confirm that JSON has 'enum' key
+        if data[key].get('enum') is not None:  # Confirm that JSON has 'enum' key
             isEnum = True
         if isEnum:
-            class_name = key.split('.')[-1] # Keys are formated {Tag}.{Name}
+            class_name = key.split('.')[-1]  # Keys are formated {Tag}.{Name}
             values = []
-            for value in data[key].get('x-enum-values'):    # This will give us the value, identifier and description
+            for value in data[key].get('x-enum-values'):  # This will give us the value, identifier and description
                 numerical_value = value.get('numericValue')
                 # Since Python uses "None" we need to change this identifier
-                identifier = value.get('identifier') if not value.get('identifier') == "None" else "none"
+                identifier = value.get('identifier')
                 # Not every enum has a description
                 description = value.get('description') if value.get('description') is not None else ""
                 values.append({'numericValue': numerical_value,
@@ -39,7 +38,7 @@ def compile_enum_data(data):
 
 
 def generate_enums(data_json):
-    path = './generated-src/main/java/lib/enums/'
+    path = '../../../generated-src/main/java/lib/enums/'
 
     files = glob.glob(path + '*', recursive=True)
     isExist = os.path.exists(path)
@@ -56,7 +55,8 @@ def generate_enums(data_json):
     print("Generating Enums")
     # For each enum we are making a separate file
     for key in data_json:
-        template_path = "src/main/generator/templates/enum.mustache"
+        template_path = "./templates/enum.mustache"
+        isFile = os.path.isfile(template_path)
         with open(template_path, 'r') as f:
             # Render enum.mustache with enum data we collected
             rendered = chevron.render(f, data_json[key])
@@ -68,27 +68,29 @@ def generate_enums(data_json):
 
     "##################################################################"
 
+
 def compile_api_parameters(parameter_data):
     param_json = []  # List allows us to dynamically add parameters
-    imports = []     # If any parameters require enums, we will add them here for importing later
+    imports = []  # If any parameters require enums, we will add them here for importing later
+    has_query = False
     for key in parameter_data:
         param_name = key['name']
         param_desc = key['description']
         param_type = key['schema']['type']  # The data type of the parameter: str, int, lst, ...
-        in_type = key['in']     # Is is a path parameter or query parameter
+        in_type = key['in']  # Is is a path parameter or query parameter
         isArray = True if param_type == "array" else False  # Some query params allow multiple values
-        isQuery = True if in_type == "query" else False     # Used for template formatting
+        isQuery = True if in_type == "query" else False  # Used for template formatting
         enum_reference = key['schema'].get('x-enum-reference')  # Get the enum reference, if any
         # Since not every param has an associated enum, we must split this into two lines
         enum_reference = enum_reference['$ref'] if enum_reference is not None else ""
         # Reference is formatted as: /{path}/{Tag}.{Name}
-        enum_reference = enum_reference.split("/")[-1].split(".")[-1]   # Get ref name
-        array_type = key['schema']['items']['type'] if param_type == "array" else ""    # Data type within array
+        enum_reference = enum_reference.split("/")[-1].split(".")[-1]  # Get ref name
+        array_type = key['schema']['items']['type'] if param_type == "array" else ""  # Data type within array
         # We want to make sure that the inputs are the correct type
         array_assert_type = ""
         assert_type = ""
 
-        required = True if key.get('required') is True else False   # Not every param is required for endpoint
+        required = True if key.get('required') is True else False  # Not every param is required for endpoint
 
         param_json.append({'param_name': param_name,
                            'param_desc': param_desc,
@@ -102,28 +104,30 @@ def compile_api_parameters(parameter_data):
                            'array_assert_type': array_assert_type
                            })
         imports.append(enum_reference)
-    return param_json, imports
+        if isQuery:
+            has_query = True
+    return param_json, imports, has_query
 
 
 def sortParams(params):
-    i = len(params)-1
+    i = len(params) - 1
     # Recursive sort - Required params in front
     while i > 0:
         if params[i]['required']:
-            if not params[i-1]['required']:
-                temp = params[i-1]
-                params[i-1] = params[i]
+            if not params[i - 1]['required']:
+                temp = params[i - 1]
+                params[i - 1] = params[i]
                 params[i] = temp
                 sortParams(params)
-        i = i-1
+        i = i - 1
     return params
 
 
 def compile_api_data(data):
-    all_methods = {}    # Creating a dict of dicts to split files by Tag
+    all_methods = {}  # Creating a dict of dicts to split files by Tag
     for path in data:
         path_data = data[path]
-        method_name = path_data['summary'].split('.')[1]    # Summary/Endpoint is formatted {Tag}.{Name}
+        method_name = path_data['summary'].split('.')[1]  # Summary/Endpoint is formatted {Tag}.{Name}
         method_desc = path_data['description']
         # The key for further inspection is dependant on whether endpoint is get or post
         # If 'get' key doesn't exist, we know it must be 'post'
@@ -133,7 +137,8 @@ def compile_api_data(data):
             endpoint_type = 'post'
         endpoint_tag = path_data[endpoint_type]['tags'][0]
         # Gets endpoint params & enum imports
-        param_data_unsorted, import_data = compile_api_parameters(data[path][endpoint_type].get('parameters'))
+        param_data_unsorted, import_data, has_query = compile_api_parameters(
+            data[path][endpoint_type].get('parameters'))
         # Python needs required method params to appear before non-required ones
         param_data = sortParams(param_data_unsorted)
 
@@ -143,7 +148,7 @@ def compile_api_data(data):
             param_info_json = {}
             for j in i.keys():
                 param_info_json.update({j: i[j]})
-            last = True if i == param_data[-1] else False   # Used for template formatting
+            last = True if i == param_data[-1] else False  # Used for template formatting
             param_info_json.update({'last': last})
 
             param_info.append(param_info_json)
@@ -153,7 +158,9 @@ def compile_api_data(data):
                        'endpoint_type': endpoint_type,
                        'path': path,
                        'method_desc': method_desc,
-                       'param_info': param_info}
+                       'param_info': param_info,
+                       'has_query': has_query
+                       }
 
         if endpoint_tag not in all_methods:
             entry = {
@@ -172,7 +179,7 @@ def compile_api_data(data):
 
 
 def generate_api(data_json):
-    path = './generated_src/main/py/lib/api/'
+    path = '../../../generated-src/main/java/lib/api/'
 
     files = glob.glob(path + '*', recursive=True)
     isExist = os.path.exists(path)
@@ -189,25 +196,26 @@ def generate_api(data_json):
     print("Generating API")
     # For each tag we are making a separate file
     for key in data_json:
-        template_path = "src/main/py/templates/api-class.mustache"
+        template_path = "./templates/api-class.mustache"
         with open(template_path, 'r') as f:
             # Render api.mustache with method data we collected
             rendered = chevron.render(f, data_json[key])
         # Some endpoints don't have a tag, we will lump them together in a 'Misc' file
         if key == '':
             key = 'Misc'
-        api_file = open(path + key + ".py", 'x')
+        api_file = open(path + key + ".java", 'x')
         api_file.write(rendered)
         api_file.close()
-        print(key + ".py created...")
+        print(key + ".java created...")
     print("!All API files created!")
 
     "#######################################################################"
 
 
 def generate():
-    apiFile = './api-src/openapi.json'
-    with open(apiFile, encoding='utf-8') as data_file:
+    # apiFile = './api-src/openapi.json'
+    apiFile = 'C:/Users/pengu_000/Documents/Destiny 2/bungie-api-java/api-src/openapi.json'
+    with open(apiFile, 'r', encoding='utf-8') as data_file:
         rawData = json.load(data_file)
     pathData = rawData.get('paths')
     schemaData = rawData.get('components').get('schemas')
