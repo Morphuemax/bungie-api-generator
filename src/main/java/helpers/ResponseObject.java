95e287313980a0f4bc3a6f05085f2064084770b4
Morphuemax <morphuemax@gmail.com>
2021-12-20 18:17:15 -0500

    Initial Commit

---
+++ src/main/java/helpers/ResponseObject.java
@@ -0,0 +1,37 @@
+package helpers;
+
+import java.util.Map;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+
+public class ResponseObject<T>{
+    T Response;
+    int ErrorCode;
+    int ThrottleSeconds;
+    String ErrorStatus;
+    String Message;
+    Map<String, String> MessageData;
+    String DetailedErrorTrace;
+
+    public ResponseObject(JsonElement jsonElement){
+        Gson gson = new Gson();
+        ResponseObject<T> temp = gson.fromJson(jsonElement, ResponseObject.class);
+        this.Response = temp.getResponse();
+        this.ErrorCode = temp.getErrorCode();
+        this.ThrottleSeconds = temp.getThrottleSeconds();
+        this.ErrorStatus = temp.getErrorStatus();
+        this.Message = temp.getMessage();
+        this.MessageData = temp.getMessageData();
+        this.DetailedErrorTrace = temp.getDetailedErrorTrace();
+    }
+
+    public T getResponse(){
+        return Response;
+    }
+    public int getErrorCode(){ return ErrorCode;}
+    public int getThrottleSeconds() { return ThrottleSeconds; }
+    public String getErrorStatus() { return ErrorStatus; }
+    public String getMessage() { return Message; }
+    public Map<String, String> getMessageData() { return MessageData; }
+    public String getDetailedErrorTrace() { return DetailedErrorTrace; }
+}
---
+++ src/test/java/TestApiCalls.java
@@ -0,0 +1,8 @@
+public class TestApiCalls {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        String fqn = TestApiCalls.class.getName();
+        System.out.println("Name: "+ fqn);
+        System.out.println("Class: "+TestApiCalls.class);
+    }
+}
