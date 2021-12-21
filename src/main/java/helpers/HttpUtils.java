bff56857dbe200f25aed94bfe4d322fd3fb465fa
Mike Pullman <Morphuemax@gmail.com>
2021-12-20 21:39:31 -0500

    Initial Commit

---
+++ src/main/java/helpers/HttpUtils.java
@@ -0,0 +1,112 @@
+package Helpers;//*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package com.mycompany.bungieapi_library.Utilities.HttpUtilities;
+//
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author pengu_000
+ */
+
+public class HttpUtils {
+
+    private static final String HOST = "https://www.bungie.net/Platform";
+    private static String apiKey;
+    private OAuth oAuth;
+
+    public HttpUtils(String apiKey) {
+        new HttpUtils(apiKey, null);
+    }
+
+    public HttpUtils(String apiKey, OAuth oAuth) {
+        this.apiKey = apiKey;
+        this.oAuth = oAuth;
+    }
+
+    protected JsonObject getBungieEndpoint(String endpoint) throws IOException {
+        URL obj = new URL(endpoint);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        // Set header
+        con.setRequestProperty("X-API-KEY", apiKey);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to Bungie.Net : " + con.getURL().toString());
+        System.out.println("Response Code : " + responseCode);
+
+        String response = getRequest(con);
+
+        JsonParser parser = new JsonParser();
+        JsonObject json = (JsonObject) parser.parse(response);
+
+        return json;
+    }
+
+    protected JsonObject postBungieEndpoint(String endpoint, String requestBody) throws IOException {
+        URL obj = new URL(endpoint);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestProperty("X-API-KEY", apiKey);
+        con.setRequestProperty("Content-Type", "application/json");
+        try {
+            con.setRequestProperty("Authorization", "Bearer " + oAuth.getAccessToken());
+        } catch (Exception e) {
+            System.out.println("OAuth Access Token not available.  OAuth may not have been initialized.");
+        }
+
+        con.setDoOutput(true);
+
+        HttpUtils.addRequestBody(con, requestBody);
+
+        String response = postRequest(con);
+        JsonParser parser = new JsonParser();
+        JsonObject json = (JsonObject) parser.parse(response);
+
+        return json;
+    }
+
+    public static String getRequest(HttpURLConnection con) throws IOException {
+        con.setRequestMethod("GET");
+        return sendRequest(con);
+    }
+
+    public static String postRequest(HttpURLConnection con) throws IOException {
+        con.setRequestMethod("POST");
+        return sendRequest(con);
+    }
+
+    private static String sendRequest(HttpURLConnection con) {
+        String response;
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+            response = "";
+            while ((inputLine = in.readLine()) != null) {
+                response += inputLine;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return response;
+    }
+
+    protected static void addRequestBody(HttpURLConnection con, String body) {
+        try (OutputStream os = con.getOutputStream()) {
+            byte[] input = body.getBytes(StandardCharsets.UTF_8);
+            os.write(input, 0, input.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/helpers/OAuth.java
@@ -0,0 +1,64 @@
+package Helpers;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
+
+public class OAuth {
+    private JsonObject oAuthJson;
+    private String clientId;
+    private String clientSecret;
+
+    public OAuth(String clientId, String clientSecret) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+    }
+
+    public void Authorize(HttpServletResponse response) throws IOException {
+        //TODO: Generate random "state"
+        response.sendRedirect("https://www.bungie.net/en/OAuth/Authorize?client_id=" + clientId + "&response_type=code");
+    }
+
+    public int Access(String authCode) throws IOException {
+        String url = "https://www.bungie.net/Platform/app/oauth/token/";
+        String urlParameters = "grant_type=authorization_code&code=" + authCode;
+        String credentials = clientId + ":" + clientSecret;
+        String encodedCredentials = Base64.getUrlEncoder().encodeToString(credentials.getBytes());
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty("Authorization", "Basic " + encodedCredentials);
+        System.out.println("Sending 'POST' request to Bungie.Net : " + url);
+        con.setDoOutput(true);
+
+        Helpers.HttpUtils.addRequestBody(con, urlParameters);
+
+        String response = Helpers.HttpUtils.postRequest(con);
+        if (response != null) {
+            JsonParser parser = new JsonParser();
+            oAuthJson = (JsonObject) parser.parse(response);
+            return oAuthJson.get("membership_id").getAsInt();
+        } else {
+            return -1;
+        }
+    }
+
+    protected String getAccessToken() {
+        return oAuthJson.get("access_token").getAsString();
+    }
+
+    protected String getRefreshToken() {
+        return oAuthJson.get("refresh_token").getAsString();
+    }
+
+    public boolean Refresh() {
+        return false;
+    }
+}
