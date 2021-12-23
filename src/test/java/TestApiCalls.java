eec82561ea95ae44dc05aaf0f29d1edcdf0cae94
Morphuemax <morphuemax@gmail.com>
2021-12-23 15:10:58 -0500

    Added tests

--- src/test/java/TestApiCalls.java
+++ src/test/java/TestApiCalls.java
@@ -1,28 +1,33 @@
 import Helpers.HttpUtils;
-import com.google.gson.JsonObject;
+import lib.api.Destiny2;
 import lib.enums.BungieMembershipType;
 import lib.enums.DestinyComponentType;
-import lib.responses.DestinyProfileResponseResponse;
-import lib.api.Destiny2;
-import lib.responses.GeneralUserResponse;
-
+import lib.enums.DestinyGameVersions;
 
 import java.io.IOException;
 
 import static lib.api.Destiny2.GetProfile;
-import static lib.api.User.GetBungieNetUserById;
+import static lib.api.Destiny2.SearchDestinyPlayer;
 
 public class TestApiCalls {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World");
         String fqn = TestApiCalls.class.getName();
-        System.out.println("Name: " + fqn);
-        System.out.println("Class: " + TestApiCalls.class);
-        String apiKey = "99f1687a8ee64e66a8e8334a73513f19";
-        HttpUtils util = new HttpUtils(apiKey);
-        Long dM_id = Long.parseLong("4611686018485616648");
-        GeneralUserResponse response = GetBungieNetUserById(20639569L, util);
-        DestinyProfileResponseResponse response2 = GetProfile(dM_id, BungieMembershipType.TigerSteam, DestinyComponentType.Profiles, util);
-        DestinyProfileResponseResponse response3 = new DestinyProfileResponseResponse(util.getBungieEndpoint("/Destiny2/3/Profile/4611686018485616648/?components=100"));
+        System.out.println("Name: "+ fqn);
+        System.out.println("Class: "+TestApiCalls.class);
+        HttpUtils util = new HttpUtils("99f1687a8ee64e66a8e8334a73513f19");
+        var membership_id = 20639569L;
+        var destinyMembershipId = 4611686018485616648L;
+        var characterId = 2305843009410461773L;
+        var enumSet = DestinyGameVersions.fromType(511);
+        var response = GetProfile(destinyMembershipId, BungieMembershipType.TigerSteam, DestinyComponentType.Profiles, util);
+        var versionsOwned = response.getResponse().getProfile().getData().getVersionsOwned();
+        var versionSet = DestinyGameVersions.fromType(versionsOwned);
+        for(var ver : versionSet){
+            var a = ver.toString();
+            System.out.print(a+", ");
+        }
+        System.out.println("\b\b");
+        System.out.println("Response: " + response);
     }
 }
