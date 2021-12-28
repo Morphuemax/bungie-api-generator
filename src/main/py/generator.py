b262d585b45913582cb42e939e1015e212daf294
Morphuemax <morphuemax@gmail.com>
2021-12-28 11:36:26 -0500

    Added multithreading/processing & timeit

--- src/main/py/generator.py
+++ src/main/py/generator.py
@@ -1,8 +1,14 @@
 import os
 import glob
+import pprint
 import shutil
 import threading
+import time
+from functools import wraps
+from multiprocessing import Process
+
 import chevron
+import timeit
 
 from data_compiler import get_responses, get_methods, get_enums, get_models, compile_resources
 
@@ -145,7 +151,8 @@ def copy_helpers():
     HttpUtils = 'HttpUtils.java'
     OAuth = 'OAuth.java'
     ResponseObj = 'ResponseObject.java'
-    read_files = [HttpUtils, OAuth, ResponseObj]
+    Flow = 'OAuthFlow.java'
+    read_files = [HttpUtils, OAuth, ResponseObj, Flow]
 
     isExist = os.path.exists(write_path)
 
@@ -170,15 +177,117 @@ def copy_helpers():
     "#####################################################################"
 
 
-def generate():
-    generator_list = [generate_enums, generate_models, generate_responses, generate_api, copy_helpers]
+def thread_time_decorator(thread):
+    @wraps(thread)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        thread(*args, **kwargs)
+        end = time.perf_counter()
+        threading.current_thread().thread_duration = end - start
+    return wrapper
+
+
+generator_list = [generate_enums, generate_models, generate_responses, generate_api, copy_helpers]
+
+
+def generate_with_threads():
     compile_resources()
     print("Generating Sources:\n")
+    threadList = []
     try:
         for generator in generator_list:
-            threading.Thread(target=generator).start()
+            wrapped_thread = thread_time_decorator(generator)
+            threadList.append(threading.Thread(target=wrapped_thread))
     except:
         print("Error Creating Thread")
+
+    start_time = time.perf_counter()
+    thread_time_map = dict()
+    duration_from_decorator = 0
+    for t in threadList:
+        t.start()
+        print(f'--- Started {t.name}')
+    for t in threadList:
+        t.join()
+        print(f'--- Completed {t.name}')
+        print(f'{t.name} took {t.thread_duration} secs ')
+        duration_from_decorator += t.thread_duration
+        thread_time_map[t.name] = t.thread_duration
+    thread_time_map['Total sum'] = sum(thread_time_map.values())
+    thread_time_map['Total No of threads'] = len(threadList)
+    thread_time_map['Average thread time'] = thread_time_map['Total No of threads'] / len(threadList)
+
+    # Capture program end time
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+
+
+    print(f'Total execution time: {execution_time} secs')
+    print(f'Total no of threads: {len(threadList)}')
+    print(f'Average time: {execution_time / len(threadList)}')
+    print(f'Decorated Threads total duration: {duration_from_decorator}')
+    print(f'Decorated Average: {duration_from_decorator / len(threadList)}')
+    pprint.pprint(thread_time_map)
+    return execution_time
+
+
+def generate_without_threads():
+    compile_resources()
+    for generator in generator_list:
+        generator()
+
+
+def generate_with_mulitprocessing():
+    compile_resources()
+    print("Generating Sources:\n")
+    processList = []
+    try:
+        for generator in generator_list:
+            processList.append(Process(target=generator))
+    except:
+        print("Error Creating Thread")
+
+    start_time = time.perf_counter()
+    for t in processList:
+        t.start()
+        print(f'--- Started {t.name}')
+    for t in processList:
+        t.join()
+        print(f'--- Completed {t.name}')
+        print(f'{t.name} took {time.perf_counter()-start_time} secs ')
+
+    # Capture program end time
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+
+
+    print(f'Total execution time: {execution_time} secs')
+    return execution_time
+
+
+def get_generation_times():
+    starttime = timeit.default_timer()
+    #print("The start time is :",starttime)
+    generate_without_threads()
+    generation_in_series_time = timeit.default_timer() - starttime
+    generation_with_threads_time = generate_with_threads()
+    # generation_with_mp_time = generate_with_mulitprocessing()
+    print(f"Generation time in series: {generation_in_series_time} sec")
+    print(f'Generation time multithreading: {generation_with_threads_time} sec')
+    # print(f'Generation time multiprocessing: {generation_with_mp_time} sec')
+
+    # print(f'Enum Generation Time: {timeit.timeit(generate_enums, number=1)}')
+    # print(f'Model Generation Time: {timeit.timeit(generate_models, number=1)}')
+    # print(f'Response Generation Time: {timeit.timeit(generate_responses, number=1)}')
+    # print(f'API Generation Time: {timeit.timeit(generate_api, number=1)}')
+
+
+def generate():
+    # starttime = timeit.default_timer()
+    generate_without_threads()
+    # print(f"Generation time in series: {timeit.default_timer() - starttime} sec")
+    # get_generation_times()
+    # generate_with_threads()
 
 
 if __name__ == '__main__':
